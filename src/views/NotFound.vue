ead06975aa7125145392b8c7d2b12c87f5ffe99d
JMaylor <60825814+JMaylor@users.noreply.github.com>
2022-01-30 15:40:59 +0000

    add reroute from 404 depending on auth state

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,22 @@
-<script lang='ts' setup>
+<script lang="ts" setup>
+import { useAuthStore } from "@/stores/auth";
+const { supabase } = useAuthStore();
 
+const reRoute = computed(() => {
+  if (supabase.auth.user())
+    return {
+      to: "/",
+      text: "Go Home",
+    };
+  return {
+    to: "/signin",
+    text: "Sign In",
+  };
+});
 </script>
 <template>
-<div class="self-center font-medium text-xl">page not found</div>
+  <div class="self-center text-xl font-medium mb-4">404 - Page not found</div>
+  <router-link :to="reRoute.to" class="self-center">{{
+    reRoute.text
+  }}</router-link>
 </template>
