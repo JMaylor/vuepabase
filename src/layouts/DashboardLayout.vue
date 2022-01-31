51b897a55d087272d296ba8edf2852da386fa853
JMaylor <60825814+JMaylor@users.noreply.github.com>
2022-01-31 10:11:58 +0000

    fix overflow styling

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -12,14 +12,14 @@ watch(route, () => {
 </script>
 
 <template>
-  <div class="flex h-full transition-colors">
+  <div class="flex h-full w-full max-w-full transition-colors">
     <NavDrawer v-model="open" />
     <div
-      class="flex h-full max-h-full flex-grow flex-col bg-zinc-100 text-zinc-900 transition-all dark:bg-zinc-900 dark:text-zinc-100"
+      class="flex h-full max-h-full flex-grow max-w-full flex-col bg-zinc-100 text-zinc-900 transition-all dark:bg-zinc-900 dark:text-zinc-100"
       :class="{ 'lg:pl-64': open }"
     >
       <AppTopBar v-model="open" />
-      <main class="flex-grow overflow-y-auto p-4">
+      <main class="flex-grow overflow-auto p-4">
         <router-view />
       </main>
       <AppFooter />
