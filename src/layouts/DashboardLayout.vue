912c5057c3ffb340dd6832dd376ced79716eed88
JMaylor <60825814+JMaylor@users.noreply.github.com>
2022-01-31 13:02:38 +0000

    show nav drawer by default on large screen

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -4,10 +4,14 @@ import { breakpointsTailwind } from "@vueuse/core";
 /* after navigation on small screens, close the nav drawer */
 const breakpoints = useBreakpoints(breakpointsTailwind);
 const lgAndLarger = breakpoints.greater("lg");
-const open = ref(true);
+const open = ref(false);
 const route = useRoute();
 watch(route, () => {
   if (!lgAndLarger.value) open.value = false;
+});
+
+onMounted(() => {
+  if (lgAndLarger.value) open.value = true;
 });
 </script>
 
@@ -15,11 +19,11 @@ watch(route, () => {
   <div class="flex h-full w-full max-w-full transition-colors">
     <NavDrawer v-model="open" />
     <div
-      class="flex h-full max-h-full flex-grow max-w-full flex-col bg-zinc-100 text-zinc-900 transition-all dark:bg-zinc-900 dark:text-zinc-100"
+      class="flex h-full max-h-full max-w-full flex-grow flex-col bg-zinc-100 text-zinc-900 transition-all dark:bg-zinc-900 dark:text-zinc-100"
       :class="{ 'lg:pl-64': open }"
     >
       <AppTopBar v-model="open" />
-      <main class="flex-grow overflow-auto p-4">
+      <main class="flex-grow overflow-auto py-4 px-6">
         <router-view />
       </main>
       <AppFooter />
